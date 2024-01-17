d0a591fed7f09632b148831e5869531ee7900ef0
SAI PREETHAM DAMIREDDY <saipreetham100@gmail.com>
2024-01-16 23:43:15 -0500

    Fix Fibroute issue

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,20 +1,15 @@
 // Endpoint for querying the fibonacci numbers
-
-
+import {Request,Response} from "express";
 import fibonacci from "./fib";
-import { Request, Response } from "express";
 
-export default (req: Request, res: Response) => {
+export default (req: Request, res:Response) => {
   const { num } = req.params;
 
-  const fibN: number = fibonacci(parseInt(num));
-  let result: string;
+  const fibN = fibonacci(parseInt(num));
+  let result = `fibonacci(${num}) is ${fibN}`;
 
-  if (fibN < 0 || isNaN(fibN)) {
-    result = fibonacci(${num}) is undefined;
-  }
-  else if(fibN>=0 && !isNaN(fibN)){
-    result=fibonacci(${num}) is ${fibN};
+  if (fibN < 0) {
+    result = `fibonacci(${num}) is undefined`;
   }
 
   res.send(result);
